96b31691c0dfeb45b146a15f29056a684ca9e6ae
Eric Nelson Elem <eric.elem@outlook.com>
2017-07-13 14:31:28 +0300

    Add display lists and items

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash, _app_ctx_stack
-from src.modals import TheUser
+from src.modals import TheUser, Bucket, Item
 registeredusers = {}
 app.secret_key = 'ggtsha6667jshjhsaknks9'
 
@@ -32,7 +32,30 @@ def register():
      
 @app.route('/lists', methods=['GET','POST'])
 def lists():
-    if session['active_user'] is None:
-        return redirect('/login')
+    if request.method=='POST':
+        registeredusers[session['active_user']].add_bucket(Bucket(request.form['listname']))
+        return redirect('/lists')
     else:
-        return render_template('lists.html',buckets=registeredusers[session['active_user']].get_buckets())
+        if session['active_user'] is None:
+            return redirect('/login')
+        else:
+            return render_template('lists.html',buckets=registeredusers[session['active_user']].get_buckets())
+
+@app.route('/items/<listname>', methods=['GET','POST'])
+def listshandler(listname):
+    if request.method=='POST':
+        registeredusers[session['active_user']].get_buckets()[listname].add_item(Item(request.form['itemname']))
+        return redirect('/items/'+listname)
+    else:
+        return render_template('items.html',bucket=listname,items=registeredusers[session['active_user']].get_buckets()[listname].get_items())
+
+@app.route('/items', methods=['GET','POST'])
+def items():
+    if request.method=='POST':
+        registeredusers[session['active_user']].get_buckets()[request.form['bucketname']].add_item(Item(request.form['itemname']))
+        return redirect('/lists')
+    else:
+        if session['active_user'] is None:
+            return redirect('/login')
+        else:
+            return render_template('items.html')
