766f536ebc81d9e3095f76f71b1cbe81093c8687
Eric Nelson Elem <eric.elem@outlook.com>
2017-07-14 01:51:59 +0300

    Add list and item functionality

--- app/views.py
+++ app/views.py
@@ -29,33 +29,71 @@ def register():
             return 'Passwords dont match'
     else:
         return render_template('register.html')
-     
+
 @app.route('/lists', methods=['GET','POST'])
-def lists():
-    if request.method=='POST':
-        registeredusers[session['active_user']].add_bucket(Bucket(request.form['listname']))
-        return redirect('/lists')
-    else:
-        if session['active_user'] is None:
-            return redirect('/login')
+@app.route('/lists/<active_bucket>', methods=['GET','POST'])
+def lists(active_bucket=None):
+    if 'active_user' in session:
+        if request.method=='POST':
+            if request.form['submit']=='Add':
+                registeredusers[session['active_user']].add_bucket(Bucket(request.form['listname']))
+            else:
+                registeredusers[session['active_user']].get_buckets()[request.form['listname']]=registeredusers[session['active_user']].get_buckets().pop(active_bucket)
+            return redirect('/lists')
         else:
-            return render_template('lists.html',buckets=registeredusers[session['active_user']].get_buckets())
-
+            if not 'active_user' in session:
+                return redirect('/login')
+            else:
+                return render_template('lists.html',bucket=active_bucket,buckets=registeredusers[session['active_user']].get_buckets())
+    else:
+        abort(403)
+@app.route('/lists/<listname>/<action>', methods=['GET','POST'])
+def modify_lists(listname,action):
+    if 'active_user' in session:
+        if action=="delete":
+            del registeredusers[session['active_user']].get_buckets()[listname]
+            return redirect('/lists')
+        elif action=="edit":
+            return redirect('/lists/'+listname)
+    else:
+        abort(403)
 @app.route('/items/<listname>', methods=['GET','POST'])
-def listshandler(listname):
-    if request.method=='POST':
-        registeredusers[session['active_user']].get_buckets()[listname].add_item(Item(request.form['itemname']))
-        return redirect('/items/'+listname)
-    else:
-        return render_template('items.html',bucket=listname,items=registeredusers[session['active_user']].get_buckets()[listname].get_items())
-
-@app.route('/items', methods=['GET','POST'])
-def items():
-    if request.method=='POST':
-        registeredusers[session['active_user']].get_buckets()[request.form['bucketname']].add_item(Item(request.form['itemname']))
-        return redirect('/lists')
-    else:
-        if session['active_user'] is None:
-            return redirect('/login')
+@app.route('/items/<listname>/<itemname>', methods=['GET','POST'])
+@app.route('/items/<listname>/<itemname>/<action>', methods=['GET','POST'])
+def listshandler(listname=None,itemname=None,action=None):
+    if 'active_user' in session:
+        if request.method=='POST':
+            if request.form['submit']=='Add':
+                registeredusers[session['active_user']].get_buckets()[listname].add_item(Item(request.form['itemname']))
+            else:
+                registeredusers[session['active_user']].get_buckets()[listname].get_items()[request.form['itemname']]=registeredusers[session['active_user']].get_buckets()[listname].get_items().pop(itemname)
+                registeredusers[session['active_user']].get_buckets()[listname].get_items()[request.form['itemname']].status=request.form['status']
+            return redirect('/items/'+listname)
         else:
-            return render_template('items.html')
+            if not action is None:
+                #return render_template('items.html',bucket=listname,items=registeredusers[session['active_user']].get_buckets()[listname].get_items(),item=None)
+                if action=='delete':
+                    del registeredusers[session['active_user']].get_buckets()[listname].get_items()[itemname]
+                    return redirect('/items/'+listname)
+                elif action=='edit':
+                    #return redirect('/items/'+listname+'/'+itemname)
+                    return render_template('items.html',bucket=listname,items=registeredusers[session['active_user']].get_buckets()[listname].get_items(),item=itemname)
+                else:
+                    return redirect('/items/'+listname)
+            else:
+                return render_template('items.html',bucket=listname,items=registeredusers[session['active_user']].get_buckets()[listname].get_items(),item=itemname)
+    else:
+        abort(403)
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    if 'active_user' in session:
+        session.pop('active_user',None)
+    return redirect('/login')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '<h1>Error 404</h1><h3>Page does not exist on this Bucketlist application</h3>'
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return '<h1>Error 403</h1><h3>You are not authorized to view this page</h3>'
